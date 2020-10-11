5a3fb854f7083d53e7dcb0d6b0d714882a70f489
jholty66 <burholtcharlie@gmail.com>
2020-10-11 12:34:52 +0100

    Added post install script.

--- gentoo-post-install.sh
+++ gentoo-post-install.sh
@@ -1,44 +1,42 @@
 #!/bin/sh
-### Install git
-time emerge --ask=n dev-vcs/git
+### Custom variables
+GITDIR=$(pwd)
+USER=charile
 
-### Install emacs
+### Add users with sudo privlages
+emerge --ask=n sudo
+nano /etc/sudoers
+useradd -m -G wheel,audio,video $USER
+passwd $USER
+
+### Setup Emacs
 # Add emacs use fag to "/etc/portage/make.conf"
 time emerge --ask=n emacs
-# TODO clone emacs config to "~/.emacsd.d/"
-
-### Add a user
-useradd -m -G wheel,audio,video admin
-passwd admin
-emacs /etc/sudoers # Fix sudo command
-su admin
+git clone https://jholty66/.emacs.d.git /home/$USER/.emacs.d/
 
 ### Change portage to use git instead of rsync
-# TODO add gentoo.conf file to the "gentoo-installer" directory
-# cp $gitdr/gentoo.conf /etc/portage/repos.conf/gentoo.conf
-# mv /var/db/repos/gentoo{,.bak}
-# mkdir /var/db/repos/gentoo
-# emerge --sync
-# # Remove "/var/db/repos/gentoo.bak" manually
+cp $GITDR/gentoo.conf /etc/portage/repos.conf/gentoo.conf
+mv /var/db/repos/gentoo{,.bak}
+mkdir /var/db/repos/gentoo
+emerge --sync
+# Remove "/var/db/repos/gentoo.bak" manually
 
+### Portage optimizations
 emerge ccache app-portage/cpuid2cpuflags sys-devel/distcc
+
+### Ccache
 mkdir -p /var/cache/ccache
 chown root:portage /var/cache/ccache
 chmod 2775 /var/cache/ccache
-
-# /var/cache/ccache/ccachd.conf
-max_size = 100.0G
-umask = 002
-compiler_check = %compiler% -v
-cache_dir_levels = 3
-
-# /etc/portage/make.conf
-FEATURES="ccache"
-CCACHE_DIR="/var/cache/ccache"
-
-# add to /etc/profile:
-export PATH="/usr/lib/ccache/bin${PATH:+:}$PATH"
-export CCACHE_DIR="/var/cache/ccache"
+cp $GITDIR/ccache.conf /var/cache/ccache/ccache.conf
+echo ""
+echo "Add the following toe /etc/portage/make.conf:"
+echo ""
+echo "FEATURES=\"ccache\""
+echo "CCACHE_DIR=\"/var/cache/ccache\""
+touch /etc/profile.d/ccache
+echo "export PATH=\"/usr/lib/ccache/bin${PATH:+:}$PATH\"" > /etc/profile.d/ccache
+echo "export CCACHE_DIR=\"/var/cache/ccache\"" > /etc/profile.d/ccache
 
 # cpuid2cpuflags
 cpuid2cpuflags
