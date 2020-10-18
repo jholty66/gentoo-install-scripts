10ba9f245c016e01b40f553d76e9d9737c8ef684
jholty66 <burholtcharlie@gmail.com>
2020-10-18 19:52:48 +0100

    f

--- gentoo-post-install.sh
+++ gentoo-post-install.sh
@@ -27,6 +27,7 @@ time emerge --ask=n emacs
 git clone https://gitub.com/jholty66/.emacs.d.git /home/$USER/.emacs.d/
 
 ### Change portage to use git instead of rsync
+mv /etc/portage/repos.conf/gentoo.conf /etc/portage/repos.conf/gentoo.conf.def
 cp $GITDR/gentoo.conf /etc/portage/repos.conf/gentoo.conf
 mv /var/db/repos/gentoo{,.bak}
 mkdir /var/db/repos/gentoo
@@ -49,6 +50,7 @@ echo "CCACHE_DIR=\"/var/cache/ccache\""
 touch /etc/profile.d/ccache
 echo "export PATH=\"/usr/lib/ccache/bin${PATH:+:}$PATH\"" > /etc/profile.d/ccache
 echo "export CCACHE_DIR=\"/var/cache/ccache\"" > /etc/profile.d/ccache
+. /etc/profile.d/ccache.sh
 
 # cpuid2cpuflags
 cpuid2cpuflags
