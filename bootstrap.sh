b494d9fb2207b4f7a897b021db56e746aede0d88
jholty66 <burholtcharliegmail.com>
2021-01-02 19:31:15 +0000

    tidy code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,18 @@
 #!/bin/sh
-
+set -e
 GITDIR=$(pwd)
 source $GITDIR/custom.sh
 alias cp="cp -v"
-
-### Install tarball
-echo "
-Dowloading tarball.
-"
 latest_stage3=$(curl http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3.txt 2>/dev/null | grep -o ^.*stage3-amd64-systemd.*\.tar\.xz)
 wget http://distfiles.gentoo.org/releases/amd64/autobuilds/$latest_stage3
-echo "
-Extracting tarball.
-"
 time tar xpvf $GITDIR/stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner -C /mnt/gentoo
-
-### Configure portage.
-echo "
-Configuring portage.
-"
-
-# package.use
 [ -d "/mnt/gentoo/etc/portage/package.use/" ] && rm -rf /mnt/gentoo/etc/portage/package.use/
 cp $GITDIR/package.use /mnt/gentoo/etc/portage/
-
-# make.conf
 [ -f "/etc/portage/make.conf" ] && cp /etc/portage/make.conf /mnt/gentoo/etc/portage/make.conf.def
 [ -f "/mnt/gentoo/etc/portage/make.conf" ] && rm /mnt/gentoo/etc/portage/make.conf
 cp -f $GITDIR/make.conf /mnt/gentoo/etc/portage/
 echo "${MAKE_CONF}" >> /mnt/gentoo/etc/portage/make.conf
-
 cp $GITDIR/{package.accept_keywords,package.license} /mnt/gentoo/etc/portage/
-
-### Chroot
-echo "
-Changing root."
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
@@ -42,5 +20,4 @@ sh mount-virtual.sh # Mounting virtual filesystems is done with a script to make
 SCRIPTS=$(cd $GITDIR && ls {chroot.sh,custom.sh,genfstab.sh})
 cp $GITDIR/$SCRIPTS /mnt/gentoo/root/
 chmod +x /mnt/gentoo/root/$SCRIPTS
-# chroot /mnt/gentoo/ /mnt/gentoo 
 env -i HOME=/root TERM=$TERM chroot /mnt/gentoo/ /root/chroot.sh
--- chroot.sh
+++ chroot.sh
@@ -1,38 +1,21 @@
 #!/bin/sh
-
+set -e
 /usr/sbin/env-update
 source /etc/profile
 source /root/custom.sh
-
-### Syncronise portage.
-echo -e "\nUpdating portage.\n"
 time (emerge-webrsync && $EMERGE --update --deep --newuse @world)
-
-### Configuration.
-echo -e "\nConfiguraing timezone and locale."
 echo "Europe/London" > /etc/timezone
 emerge --ask=n --config sys-libs/timezone-data
 echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 env-update && source /etc/profile; source /root/custom.sh
-
-### fstab
-# genfstab, from the arch-install-scripts
-# https://github.com/archlinux/arch-install-scripts, is used to create
-# the fstab file. This is more reliable and is easier to work with when
-# installing gentoo on different hardware.
-echo -e "\nCreating fstab\n"
 $EMERGE app-portage/layman dev-vcs/git
 layman -L
 yes | layman -a zscheile
 $EMERGE --nodeps arch-install-scripts asciidoc
-cp /etc/fstab{,.def}
-echo -e "\nGenerating fstab.\n"
-genfstab -U -p / >> /etc/fstab
+cp /etc/fstab /etc/fstab.def /etc/fstab.bak
+genfstab -U -p / > /etc/fstab
 cat /etc/fstab
-
-### Kernel
-echo -e "\nEmerging kernel sources.\n"
 $EMERGE sys-kernel/gentoo-sources sys-kernel/genkernel linux-firmware
 echo -e "\nCompiling kernel.\n"
 DIR=$(pwd)
@@ -40,24 +23,12 @@ cd /usr/src/linux
 make menuconfig
 time (echo $GENKERNEL | source /dev/stdin)
 cd $DIR
-
-### Tools and services.
-echo -e "\nEmerging system tools.\n"
 $EMERGE sysklogd cronie mlocate dosfstools dhcpcd gentoolkit $TOOLS
-echo -e "\nEnabling services at startup\n"
 systemctl enable cronie
 systemctl enable dhcpcd
 systemctl enable sshd
 systemctl enable sysklogd
 $SERVICES
-
-### Root password
-echo -e "\nCreate root passowrd\n"
 passwd
-
-### Bootloader
-# TODO: Learn how to install efistub
-# $INSTALL_BOOTLOADER
-
 $EMERGE efibootmgr
 install_bootloader
